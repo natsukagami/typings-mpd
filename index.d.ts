f6a2ed1555b89e84747dd999473106aa18e5b399
Nguyen Pham Cao <natsukagami@gmail.com>
2018-03-14 21:42:06 +0700

    Change callback signature

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,55 @@
-import {EventEmitter} from 'events';
+import { EventEmitter } from "events";
 
-declare module 'mpd';
+declare module "mpd";
 
 interface MpdConfig {
   host: string;
   port: number;
 }
 
-type SystemName =|'database'|'update'|'stored_playlist'|'playlist'|'player'|
-    'mixer'|'output'|'options'|'sticker'|'subscription'|'message';
+type SystemName =
+  | "database"
+  | "update"
+  | "stored_playlist"
+  | "playlist"
+  | "player"
+  | "mixer"
+  | "output"
+  | "options"
+  | "sticker"
+  | "subscription"
+  | "message";
 
 declare class mpd extends EventEmitter {
   constructor();
 
-  sendCommand(command: mpd.Command|string, callback: (response: string) => any):
-      void;
+  sendCommand(
+    command: mpd.Command | string,
+    callback: (err: any, response: string) => any
+  ): void;
 
   sendCommands(
-      commandList: (mpd.Command|string)[],
-      callback: (response: string) => any): void;
+    commandList: (mpd.Command | string)[],
+    callback: (err: any, response: string) => any
+  ): void;
 
-  on(event: 'error', callback: (err: Error) => any): this;
-  on(event: 'end', callback: () => any): this;
-  on(event: 'connect', callback: () => any): this;
-  on(event: 'ready', callback: () => any): this;
-  on(event: 'system', callback: (systemName: SystemName) => any): this;
+  on(event: "error", callback: (err: Error) => any): this;
+  on(event: "end", callback: () => any): this;
+  on(event: "connect", callback: () => any): this;
+  on(event: "ready", callback: () => any): this;
+  on(event: "system", callback: (systemName: SystemName) => any): this;
 
-  on(event: 'system-database', callback: () => any): this;
-  on(event: 'system-update', callback: () => any): this;
-  on(event: 'system-stored_playlist', callback: () => any): this;
-  on(event: 'system-playlist', callback: () => any): this;
-  on(event: 'system-player', callback: () => any): this;
-  on(event: 'system-mixer', callback: () => any): this;
-  on(event: 'system-output', callback: () => any): this;
-  on(event: 'system-options', callback: () => any): this;
-  on(event: 'system-sticker', callback: () => any): this;
-  on(event: 'system-subscription', callback: () => any): this;
-  on(event: 'system-message', callback: () => any): this;
+  on(event: "system-database", callback: () => any): this;
+  on(event: "system-update", callback: () => any): this;
+  on(event: "system-stored_playlist", callback: () => any): this;
+  on(event: "system-playlist", callback: () => any): this;
+  on(event: "system-player", callback: () => any): this;
+  on(event: "system-mixer", callback: () => any): this;
+  on(event: "system-output", callback: () => any): this;
+  on(event: "system-options", callback: () => any): this;
+  on(event: "system-sticker", callback: () => any): this;
+  on(event: "system-subscription", callback: () => any): this;
+  on(event: "system-message", callback: () => any): this;
 }
 
 declare namespace mpd {
@@ -50,7 +63,7 @@ declare namespace mpd {
 
   function connect(config: MpdConfig): mpd;
 
-  function parseKeyValueMessage(msg: string): {[x: string]: any};
+  function parseKeyValueMessage(msg: string): { [x: string]: any };
 }
 
 export = mpd;
